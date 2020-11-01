8a2d4aec8feff45a3c6d03dad8095a8b8bc35db2
cpccharry <harry.subramaniam@cpcc.edu>
2020-11-01 00:09:32 -0400

    First release.

---
+++ src/main/java/edu/lab/dockerdemo/SpringbootDockerStarterApplication.java
@@ -0,0 +1,13 @@
+package edu.lab.dockerdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDockerStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDockerStarterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/lab/dockerdemo/SpringbootDockerStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.lab.dockerdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDockerStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
